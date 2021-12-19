6673de4a03d2eff948643a7c599b47ba2e24d7b6
Mohammed Qasedo <qasedo2000@gmail.com>
2021-12-19 22:45:26 +0200

    add load balancer for order server

--- OrderServer/Controllers/OrderController.cs
+++ OrderServer/Controllers/OrderController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using OrderServer.Models;
 using OrderServer.Services;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +13,39 @@ namespace OrderServer.Controllers
     public class OrderController : ControllerBase
     {
         private readonly IOrderService _orderService;
+        private int _roundRoubinCounterForCatalogServer = 1;
+        private List<string> _catalogServersIps = new List<string>();
 
         public OrderController(IOrderService orderService)
         {
             _orderService = orderService;
+
+            _catalogServersIps.Add("http://192.168.56.105");
+            _catalogServersIps.Add("http://192.168.56.107");
         }
 
         [HttpPost]
         [Route("purchase/{id}")]
         public async Task<ActionResult<Order>> PurchaseOrder([FromRoute]int id)
         {
+            _roundRoubinCounterForCatalogServer++;
+
+            if (_roundRoubinCounterForCatalogServer == int.MaxValue) _roundRoubinCounterForCatalogServer = 1;
+
+            string catalogServerIp = string.Empty;
+
+            if (_roundRoubinCounterForCatalogServer % 2 == 0)
+            {
+                catalogServerIp = _catalogServersIps[0];
+            }
+            else
+            {
+                catalogServerIp = _catalogServersIps[1];
+            }
+
             using (var httpClient = new HttpClient())
             {
-                using (var httpClientResponse = await httpClient.GetAsync($"http://192.168.56.105/info/{id}"))
+                using (var httpClientResponse = await httpClient.GetAsync($"{catalogServerIp}/info/{id}"))
                 {
                     if (!httpClientResponse.IsSuccessStatusCode)
                     {
@@ -33,24 +54,27 @@ namespace OrderServer.Controllers
                 }
             }
 
-            using (var client = new HttpClient())
+            _catalogServersIps.ForEach(async server =>
             {
-                using (var request = new HttpRequestMessage(HttpMethod.Patch, "http://192.168.56.105/update"))
+                using (var client = new HttpClient())
                 {
-                    var json = JsonConvert.SerializeObject(new { Id = id });
-                    using (var stringContent = new StringContent(json, Encoding.UTF8, "application/json"))
+                    using (var request = new HttpRequestMessage(HttpMethod.Patch, $"{server}/update"))
                     {
-                        request.Content = stringContent;
-
-                        using (var response = await client
-                            .SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
-                            .ConfigureAwait(false))
+                        var json = JsonConvert.SerializeObject(new { Id = id });
+                        using (var stringContent = new StringContent(json, Encoding.UTF8, "application/json"))
                         {
-                            response.EnsureSuccessStatusCode();
+                            request.Content = stringContent;
+
+                            using (var response = await client
+                                .SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                                .ConfigureAwait(false))
+                            {
+                                response.EnsureSuccessStatusCode();
+                            }
                         }
                     }
                 }
-            }
+            });
 
             var orderToPurchase = new Order()
             {
