a875f940c64d60ef4001ef5582ac7d36cf0cc86f
Suhaib Hamdallah <suhaib.hamdallah@gmail.com>
2021-12-19 22:59:44 +0200

    create client project & models

---
+++ Client/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Client.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Topic { get; set; }
+        public int NumberOfItemsInStock { get; set; }
+        public decimal Price { get; set; }
+    }
+}
